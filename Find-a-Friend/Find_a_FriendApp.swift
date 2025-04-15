67973d47616741c59cc86ff16bb48adf58f2b477
dyuan67 <dyuan@villanova.edu>
2025-04-15 13:06:45 -0400

    Initial upload/commit of files

---
+++ Find-a-Friend/Find_a_FriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_a_FriendApp.swift
+//  Find-a-Friend
+//
+//  Created by Deana Yuan on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Find_a_FriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Find-a-FriendTests/Find_a_FriendTests.swift
@@ -0,0 +1,17 @@
+//
+//  Find_a_FriendTests.swift
+//  Find-a-FriendTests
+//
+//  Created by Deana Yuan on 4/15/25.
+//
+
+import Testing
+@testable import Find_a_Friend
+
+struct Find_a_FriendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
